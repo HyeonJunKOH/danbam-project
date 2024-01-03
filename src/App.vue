3fdb02836631cbdff4ae845fd80d8eccb7cdecb0
HyeonJunKOH <hyeonjun9194@gmail.com>
2024-01-03 17:18:00 +0900

    1월 3일 단밤 프로젝트 시작

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <DanBamHeader></DanBamHeader>
+    <DanBamBody></DanBamBody>
+  </div>
+</template>
+
+<script>
+import DanBamHeader from './components/DanBamHeader.vue'
+import DanBamBody from './components/DanBamBody.vue';
+
+  export default {
+    components: {
+      DanBamHeader,
+      DanBamBody
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/DanBamHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="wrap">
+        <div class="head_wrap">
+            <div class="head">
+                <span><h1>DANBAM COFEE</h1></span>
+                <span>단밤 스토리</span>
+                <span>메뉴 소개</span>
+                <span>메뉴 주문</span>
+                <span>매장</span>
+                <span>단밤 소식</span>
+                <span>회원가입</span>
+                <span>로그인</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    div {
+        display: block;
+    }
+    .wrap{
+        width: 100%;
+    }
+    
+</style>
